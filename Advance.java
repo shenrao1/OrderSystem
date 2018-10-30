53173bddc2a79cc7684453cae4cd88bb3b213b2f
shenrao1 <44608938+shenrao1@users.noreply.github.com>
2018-10-30 21:44:14 +0800

    Add files via upload

--- Advance.java
+++ Advance.java
@@ -1,12 +1,12 @@
-package aop;
+﻿package aop;
 
 import org.aspectj.lang.JoinPoint;
 
 public class Advance {
     public void beforeTest(JoinPoint jp){
-        System.out.println("前置");
+        System.out.println("前置。。。。");
     }
     public void afterTest(JoinPoint jp){
-        System.out.println("后置");
+        System.out.println("后置。。。。");
     }
 }
